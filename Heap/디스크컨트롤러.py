9e0be42c3a08d35630ca12862394d96e929bda34
RInRInPark <surinp8019@naver.com>
2023-06-27 10:56:21 +0900

    Heap

--- Heap/디스크컨트롤러.py
+++ Heap/디스크컨트롤러.py
@@ -3,19 +3,21 @@ import heapq
 
 def solution(jobs):
     answer = 0
+    count = 0  # 모든 작업을 마쳤는지 확인
+    start = -1  # 이전 작업이 시작한 시점
+    finish = 0  # 이전 작업이 끝난 시점
     heap = []
-    count, start, finish = 0, -1, 0
 
     while count < len(jobs):
-        for j in jobs:
-            if start < j[0] <= finish:
-                heapq.heappush(heap, [j[1], j[0]])
+        for ele in jobs:
+            if start < ele[0] <= finish:
+                heapq.heappush(heap, [ele[1], ele[0]])
         if len(heap) > 0:
-            ele = heapq.heappop(heap)
-            start = finish
-            finish += ele[0]
-            answer += (finish - ele[1])
+            arr = heapq.heappop(heap)
             count += 1
+            start = finish
+            finish = finish + arr[0]
+            answer += (finish-arr[1])
         else:
             finish += 1
 
